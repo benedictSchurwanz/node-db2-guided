ffd9efac84f4b30eee28766d6eb3f71c0f80fe5c
Ladrillo <gabriel@cabrejas.net>
2021-12-07 19:47:31 +0100

    committing to "lecture"

--- data/seeds/make-fruits.js
+++ data/seeds/make-fruits.js
@@ -1,11 +1,33 @@
-exports.seed = function (knex) {
+exports.seed = async function (knex) {
   // Truncating a table deletes ALL existing entries and resets the primary keys
-  return knex('fruits').truncate()
-    .then(function () {
-      return knex('fruits').insert([
-        { fruit_name: 'apple', avg_weight_oz: 2.5, delicious: 1 },
-        { fruit_name: 'pear', avg_weight_oz: 4.5, delicious: 1 },
-        { fruit_name: 'pineapple', avg_weight_oz: 7.5, delicious: 0 }
-      ]);
-    });
+  await knex('fruits').truncate()
+  await knex('fruits').insert([
+    { fruit_name: 'apple', avg_weight_oz: 2.5, delicious: 1 },
+    { fruit_name: 'pear', avg_weight_oz: 4.5, delicious: 1 },
+    { fruit_name: 'pineapple', avg_weight_oz: 7.5, delicious: 0 }
+  ]);
+
+
+  await knex('fruits').truncate()
+  await knex('fruits').insert([
+    { fruit_name: 'apple', avg_weight_oz: 2.5, delicious: 1 },
+    { fruit_name: 'pear', avg_weight_oz: 4.5, delicious: 1 },
+    { fruit_name: 'pineapple', avg_weight_oz: 7.5, delicious: 0 }
+  ]);
+
+
+  await knex('fruits').truncate()
+  await knex('fruits').insert([
+    { fruit_name: 'apple', avg_weight_oz: 2.5, delicious: 1 },
+    { fruit_name: 'pear', avg_weight_oz: 4.5, delicious: 1 },
+    { fruit_name: 'pineapple', avg_weight_oz: 7.5, delicious: 0 }
+  ]);
+
+
+  await knex('fruits').truncate()
+  await knex('fruits').insert([
+    { fruit_name: 'apple', avg_weight_oz: 2.5, delicious: 1 },
+    { fruit_name: 'pear', avg_weight_oz: 4.5, delicious: 1 },
+    { fruit_name: 'pineapple', avg_weight_oz: 7.5, delicious: 0 }
+  ]);
 };
