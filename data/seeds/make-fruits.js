0a5c172fa26c5315fe30399f0a10e33ed12d9433
Ladrillo <gabriel@cabrejas.net>
2021-12-07 20:01:22 +0100

    committing to "lecture"

--- data/seeds/make-fruits.js
+++ data/seeds/make-fruits.js
@@ -2,8 +2,8 @@ exports.seed = async function (knex) {
   // Truncating a table deletes ALL existing entries and resets the primary keys
   await knex('fruits').truncate()
   await knex('fruits').insert([
-    { fruit_name: 'apple', avg_weight_oz: 2.5, delicious: 1 },
-    { fruit_name: 'pear', avg_weight_oz: 4.5, delicious: 1 },
+    { fruit_name: 'apple', avg_weight_oz: 2.5, delicious: 1, color: 'red' },
+    { fruit_name: 'pear', avg_weight_oz: 4.5, delicious: 1, color: 'green' },
     { fruit_name: 'pineapple', avg_weight_oz: 7.5, delicious: 0 }
   ]);
 };
