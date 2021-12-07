8124e31015dbca8ca865a1d06c0e47965cd15afc
Ladrillo <gabriel@cabrejas.net>
2021-12-07 19:41:28 +0100

    committing to "lecture"

--- data/seeds/make-fruits.js
+++ data/seeds/make-fruits.js
@@ -1,12 +1,12 @@
 exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+  // Truncating a table eletes ALL existing entries
+  return knex('fruits').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        { id: 1, colName: 'rowValue1' },
-        { id: 2, colName: 'rowValue2' },
-        { id: 3, colName: 'rowValue3' }
+      return knex('fruits').insert([
+        { colName: 'rowValue1' },
+        { colName: 'rowValue2' },
+        { colName: 'rowValue3' }
       ]);
     });
 };
