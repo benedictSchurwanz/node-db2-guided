5c8717d8ef6edaf44fb81f90872ac587b3318786
Ladrillo <gabriel@cabrejas.net>
2021-12-07 19:09:50 +0100

    committing to "lecture"

--- data/migrations/20211207175224_make-fruits-table.js
+++ data/migrations/20211207175224_make-fruits-table.js
@@ -2,7 +2,9 @@ exports.up = async function(knex) {
   await knex.schema.createTable('fruits', table => {
     // primary key
     table.increments('fruit_id') // defaults to 'id' if argument is omitted
-    table.text('fruit_name', 128) 
+    table.text('fruit_name', 128) // sqlite does not enforce the limit
+      .unique()
+      .notNullable()
   })
 }
 
