9a06456b7e09476e69b96dde159580ace75b809f
Ladrillo <gabriel@cabrejas.net>
2021-12-07 19:39:18 +0100

    committing to "lecture"

--- data/migrations/20211207175224_make-fruits-table.js
+++ data/migrations/20211207175224_make-fruits-table.js
@@ -1,20 +1,20 @@
-exports.up = async function(knex) {
+exports.up = async function (knex) {
   await knex.schema.createTable('fruits', table => {
     // primary key
     table.increments('fruit_id') // defaults to 'id' if argument is omitted
 
     table.text('fruit_name', 128) // sqlite does not enforce the char limit VARCHAR(50)
-    .unique()
-    .notNullable()
+      .unique()
+      .notNullable()
 
     table.decimal('avg_weight_oz')
-    .notNullable()
+      .notNullable()
 
     table.boolean('delicious')
-    .defaultTo(false)
+      .defaultTo(false)
   })
 }
 
-exports.down = async function(knex) {
+exports.down = async function (knex) {
   await knex.schema.dropTableIfExists('fruits')
 }
