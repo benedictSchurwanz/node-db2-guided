e98191545b9a0ecd02a8668735bc2111bdf4b868
Ladrillo <gabriel@cabrejas.net>
2021-12-07 19:59:45 +0100

    committing to "lecture"

--- data/migrations/20211207185843_add-color-to-fruits.js
+++ data/migrations/20211207185843_add-color-to-fruits.js
@@ -1,10 +1,12 @@
 
 exports.up = function(knex) {
   return knex.schema.table('fruits', table => {
-    
+    table.string('color', 56)
   })
 };
 
 exports.down = function(knex) {
-
+  return knex.schema.table('fruits', () => {
+    table.dropColumn('color')
+  })
 };
