5af968271118b4f7babd699b05bab290fbcd061a
Ladrillo <gabriel@cabrejas.net>
2021-12-07 18:59:38 +0100

    committing to "lecture"

--- data/migrations/20211207175224_make-fruits-table.js
+++ data/migrations/20211207175224_make-fruits-table.js
@@ -1,7 +1,7 @@
 exports.up = async function(knex) {
   await knex.schema.createTable('fruits', table => {
     // primary key
-    table.increments('fruit_id') // defaults to 'id'
+    table.increments('fruit_id') // defaults to 'id' if omitted
   })
 }
 
