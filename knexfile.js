176efc86490b1bc8ec9f136e33e9428751006f0a
Ladrillo <gabriel@cabrejas.net>
2021-12-07 18:43:09 +0100

    committing to "lecture"

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,10 @@ module.exports = {
     },
     useNullAsDefault: true,
     migrations: {
-      
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   },
   production: {},
